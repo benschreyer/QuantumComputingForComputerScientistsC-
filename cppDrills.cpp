86ffe504f4d329cfeced6dba59beb6af5450b7b9
bensch <crafte23@gmail.com>
2020-06-13 16:23:30 -0400

    Update cppDrills.cpp

--- cppDrills.cpp
+++ cppDrills.cpp
@@ -1,81 +1,89 @@
-
 // quantumdrills.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // Quantum Computing For Computer Science Drills Benjamin Schreyer
-// each drill is labeled
+
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <vector>
+
 //Drill 1.1.1
-struct complex
+struct  complex
 {
 	//real
 	double a;
 	//imaginary
 	double b;
 };
+
+
+typedef   complex complex;
+typedef std::vector<std::vector<  complex>>  complexMatrix;
 //Drill 1.3.1
-struct polarComplex
+struct  polarComplex
 {
 	double m;//magnitude
 	double t;//angle
 };
+typedef struct polarComplex polarComplex;
 //Drill 1.1.1
-struct complex complexMultiply(struct complex x, struct complex y)
+  complex complexMultiply(  complex x,   complex y)
 {
 	//definition of complex multiplication
-	struct complex o;
+	  complex o;
 	o.a = x.a * y.a - x.b * y.b;
 	o.b = x.a * y.b + y.a * x.b;
 	return o;
 }
 //Drill 1.1.1
-struct complex complexAdd(struct complex x, struct complex y)
+  complex complexAdd(  complex x,   complex y)
 {
 	//definition of complex addition
-	struct complex o;
+	  complex o;
 	o.a = x.a + y.a;
 	o.b = x.b + y.b;
 	return o;
 }
+//Drill useful
+  
 //Drill 1.2.1
-struct complex complexSubtract(struct complex x, struct complex y)
+  complex complexSubtract(  complex x,   complex y)
 {
 	//use addition but multiply the second term by (-1,0)
-	struct complex n;
+	  complex n;
 	n.a = -1.0;
 	n.b = 0.0;
 	return complexAdd(x, complexMultiply(n, y));
 }
 //Drill 1.2.1
-struct complex complexDivide(struct complex n, struct complex d)
+  complex complexDivide(  complex n,   complex d)
 {
 	//definition of complex division
-	struct complex o;
+	  complex o;
 	o.a = (n.a * d.a + n.b * d.b) / (d.a * d.a + d.b * d.b);
 	o.b = (d.a * d.b - n.a * d.b) / (d.a * d.a + d.b * d.b);
 	return o;
 };
 
 //Dril 1.2.1
-double complexModulus(struct complex x)
+double complexModulus(  complex x)
 {
 	//length of a 2d vector same as complex modulus
 	return pow(x.a * x.a + x.b * x.b, 0.5);
 }
 
 //Drill 1.3.1
-struct complex polarToCartesian(struct polarComplex x)
+  complex polarToCartesian(  polarComplex x)
 {
-	struct complex n;
+	  complex n;
 	n.a = x.m;
 	n.b = 0;
-	struct complex a;
+	  complex a;
 	a.a = cos(x.t);
 	a.b = sin(x.t);
 	return complexMultiply(n, a);
 }
 //Drill 1.3.1
-struct polarComplex cartesianToPolar(struct complex x)
+  polarComplex cartesianToPolar(  complex x)
 {
 	polarComplex o;
 	o.m = complexModulus(x);
@@ -84,29 +92,316 @@ struct polarComplex cartesianToPolar(struct complex x)
 }
 
 //Drill 1.3.1
-std::string complexToPolarString(struct complex x)
+std::string complexToPolarString(  complex x)
 {
 	return std::to_string(atan(x.b / x.a)) + "ang " + std::to_string(complexModulus(x));
 }
 //Drill 1.1.1
-std::string complexToString(struct complex x)
+std::string complexToString(  complex x)
 {
 	return std::to_string(x.a) + " + " + std::to_string(x.b) + "i";
 }
+//Drill 2.1.1/2.2.1
+complexMatrix & complexMatrixScalarMultiplication(float r, complexMatrix & m)
+{
+	for (int i = 0; i < m.size(); i++)
+	{
+		for (int j = 0; j < m[0].size(); j++)
+		{
+			m[i][j] = complexMultiply({r,0.0}, m[i][j]);
+		}
+	}
+	return m;
+}
+//Drill 2.1.1/2.2.1
+complexMatrix& printComplexMatrix(complexMatrix& m)
+{
+	//std::cout << "\n\nOOOGGGG" << m.size() << "\n\n";
+	for (int i = 0; i < m.size(); i++)
+	{
+		for (int j = 0; j < m[0].size(); j++)
+		{
+			std::cout << "(" + complexToString(m[i][j]) + ") ";
+		}
+		std::cout << "\n";
+	}
+	return m;
+}
+complexMatrix & complexMatrixAdditiveInverse(complexMatrix & m)
+{
+	complexMatrixScalarMultiplication(-1.0, m);
 
+	return m;
+}
+complexMatrix & complexMatrixAddition(complexMatrix & m, complexMatrix & n, complexMatrix & res)
+{
+	//printComplexMatrix(m);
+//	std::cout << "\n\n";
+	//printComplexMatrix(n);
+	res.resize(m.size());
+	for (int i = 0; i < m.size(); i++)
+	{
+		res[i].resize(m[0].size());
+	}
+	for (int i = 0; i < m.size(); i++)
+	{
+		for (int j = 0; j < m[0].size(); j++)
+		{
+			res[i][j] = complexAdd(m[i][j],n[i][j]);
+		}
+	}
+	return res;
+}
+
+//select row then column [][]
+//Drill 2.2.2/2.2.3
+complexMatrix& complexMatrixMultiplication(complexMatrix & a, complexMatrix & b, complexMatrix & out)
+{
+	//std::cout << "\n\n";
+	//printComplexMatrix(a);
+	//std::cout << "\n\n";
+//	printComplexMatrix(b);
+	out.resize(a.size());
+	//std::cout <<"\n\n" << a.size() <<"\n\n" << b[0].size()<<"\n\n";
+	for (int i = 0; i < out.size(); i++)
+	{
+		out[i].resize(b[0].size());
+	}
+	//std::cout << a.size() << b[0].size();
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int k = 0; k < b[0].size(); k++)
+		{
+			complex c = { 0.0,0.0 };
+			for (int j = 0; j < a[0].size(); j++)
+			{
+			//	std::cout << complexToString(b[j][k]) + "*" + complexToString(a[i][j]) + "\n\n\n";
+				c = complexAdd(c, complexMultiply(b[j][k], a[i][j]));
+			}
+			out[i][k] = c;
+		}
+	}
+
+	return out;
+}
+//Drill 2.4.1
+complexMatrix& complexMatrixConjugate(complexMatrix& a)
+{
+	//std::cout << a[0].size() << "   " << a.size();
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[0].size(); j++)
+		{
+			a[i][j] = { a[i][j].a,-1 * a[i][j].b };
+		}
+	}
+	
+	
+	return a;
+}
+complexMatrix& copyMatrix(complexMatrix& a,complexMatrix& b)
+{
+	b.resize(a.size());
+	//std::cout << a.size();
+	for (int i = 0; i < a.size(); i++)
+	{
+		b[i].resize(a[0].size());
+		for (int j = 0; j < a[0].size(); j++)
+		{
+			b[i][j] = a[i][j];
+		}
+	}
+
+	return b;
+}
+complexMatrix& complexMatrixTranspose(complexMatrix& a)
+{
+	int s = a[0].size();
+	int z = a.size();
+	complexMatrix b;
+	copyMatrix(a, b);
+	a.resize(s);
+	for (int i = 0; i < s; i++)
+	{
+		a[i].resize(z);
+	}
+
+
+	for (int i = 0; i < s; i++)
+	{
+		
+		for (int j = 0; j < z; j++)
+		{
+			a[i][j] = b[j][i];
+		}
+	}
+
+	return a;
+}
+complexMatrix& complexMatrixAdjoint(complexMatrix& a)
+{
+
+	return complexMatrixTranspose(complexMatrixConjugate(a));
+}
+complex complexTrace(complexMatrix& m)
+{
+	complex c = { 0.0,0.0 };
+	for (int i = 0; i < m.size() && i < m[0].size(); i++)
+	{
+		c = complexAdd(c, m[i][i]);
+	}
+
+	return c;
+}
+
+complex complexMatrixInnerProduct(complexMatrix& a, complexMatrix& b)
+{
+	complexMatrix g;
+	return complexTrace(complexMatrixMultiplication((a), complexMatrixAdjoint(b),g));
+}
+//Drill 2.4.2
+complex complexMatrixNorm(complexMatrix& a)
+{
+	complexMatrix d;
+	copyMatrix(a, d);
+	polarComplex c = cartesianToPolar(complexMatrixInnerProduct(a,d));
+	c.t /= 2.0;
+	c.m = pow(c.m,0.5);
+	return polarToCartesian(c);
+}
+//Drill 2.4.3
+complex complexMatrixDistance(complexMatrix& a, complexMatrix& b)
+{
+	complexMatrix g;
+	complexMatrixAddition(a, complexMatrixAdditiveInverse(b), g);
+	
+	return complexMatrixNorm(g);
+}
+//Drill 2.6.1
+bool doubleEquality(double d1, double d2)
+{
+	if (abs(d1 - d2) < 0.0001)
+	{
+		return true;
+	}
+	return false;
+}
+bool complexEquality(complex a, complex b)
+{
+	return doubleEquality(a.a,b.a) && doubleEquality(a.b, b.b);
+}
+bool complexMatrixEquality(complexMatrix& a, complexMatrix& b)
+{
+	if (a.size() == b.size() && b[0].size() == a[0].size())
+	{
+		for (int i = 0; i < a.size(); i++)
+		{
+			b[i].resize(a[0].size());
+			for (int j = 0; j < a[0].size(); j++)
+			{
+				if (!complexEquality(b[i][j], a[i][j]))
+					return false;
+			}
+		}
+		return true;
+	}
+	else
+		return false;
+}
+bool complexMatrixIsHermetian(complexMatrix& m)
+{
+	complexMatrix g;
+	copyMatrix(m, g);
+	std::cout << "\n\n";
+	printComplexMatrix(g);
+	complexMatrixAdjoint(g);
+	std::cout << "\n\n";
+	printComplexMatrix(g);
+	std::cout << "\n\n";
+	printComplexMatrix(m);
+	std::cout << "\n\n";
+	return complexMatrixEquality(m,g);
+}
+//Drill 2.6.2
+bool complexMatrixIsUnitary(complexMatrix& m)
+{
+	complexMatrix ma;
+	copyMatrix(m, ma);
+	complexMatrixAdjoint(ma);
+	complexMatrix I;
+	complexMatrix o1;
+	complexMatrix o2;
+	I.resize(m.size());
+	for (int i = 0; i < m.size(); i++)
+	{
+		I[i].resize(m[0].size());
+	}
+	for (int i = 0; i < I.size(); i++)
+	{
+		
+		for (int j = 0; j < I[0].size(); j++)
+		{
+			if (i == j)
+			{
+				I[i][j] = { 1.0,0.0 };
+
+			}
+			else 
+			{
+				I[i][j] = { 0.0,0.0 };
+			}
+		}
+	}
+	if (complexMatrixEquality(complexMatrixMultiplication(m, ma, o1), I) && complexMatrixEquality(complexMatrixMultiplication(ma, m, o2), I))
+	{
+		return true;
+	}
+	return false;
+}
+complexMatrix& complexMatrixTensorProduct(complexMatrix& a,complexMatrix& b,complexMatrix & out)
+{
+	out.resize(a.size() * b.size());
+	for(int i = 0;i < out.size();i++)
+	{
+		out[i].resize(a[0].size() * b[0].size());
+	}
+	for (int i = 0; i < out.size(); i++)
+	{
+		for (int j = 0; j < out[0].size(); j++)
+		{
+			out[i][j] = complexMultiply(a[i / b.size()][j / b[0].size()], b[i % b.size()][j%b[0].size()]);
+		}
+	}
+	return out;
+}
+double ketStateProbability(complexMatrix& ket, int x)
+{
+	return pow(complexModulus(ket[x][0]), 2) / pow(complexMatrixNorm(ket).a, 2);
+}
+complex ketStateTransitionAmplitude(complexMatrix& ketFrom, complexMatrix& ketToo)
+{
+	//complexToString();
+	complexMatrixAdjoint(ketToo);
+	complexMatrix t;
+	
+	complexMatrixMultiplication(ketToo, ketFrom, t);
+	complexMatrixAdjoint(ketToo);
+	return complexTrace(t);
+}
 int main()
 {
+
 	//Drill 1.1.1
 	std::cout << "Give two complex numbers, first the real magnitude then the complex magniute(4 total floats):\n";
-	struct complex w;
-	struct complex z;
+	complex w;
+	complex z;
 	std::cin >> w.a;
 	std::cin >> w.b;
 	std::cin >> z.a;
 	std::cin >> z.b;
 	std::cout << "\nYour given numbers: " + complexToString(w) + " | " + complexToString(z) + "\n\n";
 	std::cout << "Drill 1.1.1\n";
-	std::cout << "Multiplied: " + complexToString(complexMultiply(w, z)) + " Added: " + complexToString(complexAdd(w, z) ) + "\n\n";
+	std::cout << "Multiplied: " + complexToString(complexMultiply(w, z)) + " Added: " + complexToString(complexAdd(w, z)) + "\n\n";
 	//Drill 1.2.1
 	std::cout << "Drill 1.2.1\n";
 	std::cout << "Subracted: " + complexToString(complexSubtract(w, z)) + " Divided: " + complexToString(complexDivide(w, z)) + "\n";
@@ -114,10 +409,126 @@ int main()
 	//Drill 1.3.1
 	std::cout << "Drill 1.3.1\n";
 	std::cout << "Polar of your complex numbers: " + complexToPolarString(w) + " | " + complexToPolarString(z) + " Testing of polar to complex(will just repeat the numbers you gave):" + complexToString(polarToCartesian(cartesianToPolar(w))) + " | " + complexToString(polarToCartesian(cartesianToPolar(z))) + "\n";
+
+
+	//Drill 2.1.1/2.2.1
+	std::cout << "Drill 2.1.1/2.2.1\n";
+	complexMatrix  mat1 =
+	{ {{3,2}, {-1,0}, {2,0}},
+	{ {3,3}, {7,-6}, {0,0}},
+	{ {4,5}, {5,5}, {-1,-1}} };
+	complexMatrix  mat2 =
+	{ {{1,0}, {0,0}, {1,0}},
+	{ {0,0}, {1,0}, {0,0}},
+	{ {1,0}, {0,0}, {2,0} } };
+	std::cout << "Matrix 1:\n";
+	printComplexMatrix(mat1);
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "Matrix 2:\n";
+	printComplexMatrix(mat2);
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "Additive inverse of Matrix 1:\n";
+	complexMatrixAdditiveInverse(mat1);
+	printComplexMatrix(mat1);
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "Subtraction of Matrix 1 from Matrix 2:\n";
+	complexMatrix  addedMat;
+	complexMatrixAddition(mat1, mat2, addedMat);
+	printComplexMatrix(addedMat);
+	std::cout << "\n";
+	std::cout << "\n";
+	//Drill 2.2.2/2.2.3
+	std::cout << "Drill 2.2.2/2.2.3\nMultiplying the additive inverse by Matrix 2\n";
+	complexMatrix  multMat;
+	complexMatrixMultiplication(mat2, mat1, multMat);
+	printComplexMatrix(multMat);
+	std::cout << "\n\n";
+	//Drill 2.4.1
+	complexMatrix vec1 = { {{33,330},{0,34},{324,0}} };
+	complexMatrix vec2 = { {{-5,-1},{0,0},{4,0}} };
+	complexMatrix abc;
+	std::cout << "Drill 2.4.1 \nInner Product of complex matrices\n";
+	std::cout << complexToString(complexMatrixInnerProduct(vec1, vec2));
+	//Drill 2.4.2
+	std::cout << "\nDrill 2.4.2 \nNorm of a complex vector \n";
+	std::cout << complexToString(complexMatrixNorm(vec1));
+	//Drill 2.4.3
+	vec1 = { {{33,330},{0,34},{324,0}} };
+	vec2 = { {{-5,-1},{0,0},{4,0}} };
+
+	std::cout << "\nDrill 2.4.3 distance of two complexvectors\n";
+	std::cout << "\nFirst Vector:\n";
+	printComplexMatrix(vec1);
+	std::cout << "\n\n";
+	std::cout << "\nSecond Vector:\n";
+	printComplexMatrix(vec2);
+	std::cout << complexToString(complexMatrixDistance(vec1, vec2));
+	//Drill 2.6.1
+	vec1 =
+	{ {{1,0}, {1,3}, {4,-7}},
+	{ {1,-3}, {2,0}, {3,5.000}} ,
+	{ {4,7},{3,-5},{1,0}} };
+	if (complexMatrixIsHermetian(vec1))
+	{
+		std::cout << "\nHERMETIAN\n";
+
+	}
+	else
+	{
+		std::cout << "\nNOT HERMETIAN\n";
+	}
+	//Drill 2.6.2
+	std::cout << "\n\nDrill 2.6.2\n\n";
+	complexMatrix unitaryTest = { {{0.5,0.5} ,{0,1/pow(3,0.5)}, {1.5/pow(15,0.5),0.5 / pow(15,0.5)}},
+								{{-0.5,0.0}, {1/pow(3,0.5),0.0}, {2/pow(15,0.5),1.5/pow(15,0.5)}},
+								{ {0.5,0.0},{0.0,-1/pow(3,0.5)},{0.0,2.5/pow(15,0.5)} } };
+	if (complexMatrixIsUnitary(unitaryTest))
+	{
+		std::cout << "\nUNITARY\n";
+
+	}
+	else
+	{
+		std::cout << "\nNOT UNITARY\n";
+	}
+	//Drill 2.7.1
+	std::cout << "\n\nDrill 2.7.1\n\n";
+	complexMatrix tens1 =     { {{3,2} ,{5,-1}, {0,2}},
+								{{0.0,0.0}, {12,0.0}, {6,-3}},
+								{ {2,0.0},{4,4},{9.0,3} } };
+	complexMatrix tens2 =       { {{1,0} ,{3,4}, {5,-7}},
+								{{10,2.0}, {6,0.0}, {2,5}},
+								{ {0.0,0.0},{1.0,0},{2.0,9} } };
+	complexMatrix tout;
+	complexMatrixTensorProduct(tens1, tens2, tout);
+	std::cout << "\n\n";
+	printComplexMatrix(tout);
+	std::cout << "\n\n";
+	//Drill 4.1.1
+	complexMatrix ket1 = { { {-3,-1}, {0,-2}, {0,1}, {2,0} } };//row ket 1
+	complexMatrix ket2 = { {  {1,0}, {0,1} } };//row ket 2
+	complexMatrix ket3 = { {  {0,1}, {-1,0} } };//row ket 3
+	complexMatrixScalarMultiplication(pow(2, 0.5) / 2, ket2);
+	complexMatrixScalarMultiplication(pow(2, 0.5) / 2, ket3);
+	complexMatrix ketout;
+	std::cout << "\nDrill 4.1.1\nPosition and Transistion probabilities for kets\n";
+	complexMatrixTranspose(ket1);
+	complexMatrixTranspose(ket2);
+	complexMatrixTranspose(ket3);
+	//complexMatrixAdjoint(ket3);
+	std::cout << "Probability of finding the particle at position 2 of ket 1: " << ketStateProbability(ket1,2);
+	std::cout << "\nProbability of particle state transitioning from ket 2 to ket 3: " << complexToString(ketStateTransitionAmplitude(ket2,ket3)) ;
 	//prevent window close
 	std::cin >> z.b;
-
-
-
 }
+	
+
+
+
+
+
+
 
